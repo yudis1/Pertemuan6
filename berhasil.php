5cf88c07f18156246609af39b87322be0b319206
yudis1 <yudistiras06@gmail.com>
2017-11-09 23:13:43 +0700

    da
    daw

---
+++ berhasil.php
@@ -0,0 +1,22 @@
+<html lang="en">
+	<head>
+	<title>Login Page</title>
+	<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+<body>
+<div id="header">
+	<h1>Widget Corp Login</h1>
+	</div>
+	
+<div id="main">
+	<div id="navigation">
+	</div>
+	<div id="page">
+<h2>SELAMAT BERHASIL REGISTRASI !!! </h2>
+<a href="form_regis.php">Back</a>
+</div>
+	</div>
+<div id = "footer">Copyright 2017, Widget Corp</div>
+</form>
+</body>
+</html>
